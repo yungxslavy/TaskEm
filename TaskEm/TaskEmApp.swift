57529a764ef7187bb0dd7f6d94de47007b1d7226
yungxslavy <46117128+yungxslavy@users.noreply.github.com>
2024-06-02 21:07:21 -0500

    Initial Commit

---
+++ TaskEm/TaskEmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskEmApp.swift
+//  TaskEm
+//
+//  Created by Tomislav Vuckovic on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskEmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
