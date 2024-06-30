9cd1cf6f9ae056d8c34add5f6b7e13c5e62b489a
yungxslavy <46117128+yungxslavy@users.noreply.github.com>
2024-06-30 18:32:04 -0500

    Refactored and progressed taskcreation view

--- TaskEm/AddTaskView.swift
+++ TaskEm/AddTaskView.swift
@@ -12,125 +12,19 @@ struct AddTaskView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             ScrollView {
-                
-                // Title
-                HStack {
-                    Image(systemName: "pencil")
-                        .foregroundColor(.blue)
-                    Text("Title:")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                    Spacer()
-                }
-                
-                // Title Input
-                HStack {
-                    TextField("Enter title", text: $title)
-                        .padding(.leading, 4)
-                        .padding(.bottom, 8)
-                        .overlay(
-                            Rectangle()
-                                .frame(height: 1)
-                                .foregroundColor(Color.primary)
-                                .padding(.leading, 4),
-                            alignment: .bottom
-                        )
-                    .padding(.bottom, 15)
-                    
-                    // Icon Selection Button
-                    Button(action: {
-                        showIconPicker.toggle()
-                    }){
-                        Image(systemName: selectedIconName)
-                            .frame(width: 45, height: 45)
-                            .foregroundStyle(iconColor)
-                            .background(.ultraThinMaterial, in:RoundedRectangle(cornerRadius: 10.0))
-                            
-                    }
-                    .padding(.bottom, 15)
-                    .popover(isPresented: $showIconPicker){
-                        IconPickerView(selectedIcon: $selectedIconName, iconColor: $iconColor)
-                    }
-                }
-                
-                // Due Date
-                HStack {
-                    Image(systemName: "calendar")
-                        .foregroundColor(.blue)
-                    Text("Date:")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                    Spacer()
-                }
-                
-                // Due Date Selection
-                VStack {
-                    // Date
-                    DatePicker(
-                        "Select due date",
-                        selection: $dueDate,
-                        displayedComponents: [.date]
-                    )
-                    .datePickerStyle(WheelDatePickerStyle())
-                    .labelsHidden()
-                    
-                    // Time
-                    DatePicker(
-                        "Set a time",
-                        selection: $dueDate,
-                        displayedComponents: [.hourAndMinute]
-                    )
-                    .labelsHidden()
-                }
-                
-                // Details
-                HStack {
-                    Image(systemName: "text.alignleft")
-                        .foregroundColor(.blue)
-                    Text("Notes:")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                    Spacer()
-                }
-                // Detail placeholder overview
-                ZStack {
-                    TextEditor(text: $notes)
-                        .padding(2)
-                        .border(Color.gray, width: 2)
-                        .frame(height: 200)
-                    .cornerRadius(3)
-                    
-                    if(notes.isEmpty) {
-                        VStack {
-                            HStack {
-                                Text("Enter notes here...")
-                                    .foregroundStyle(.tertiary)
-                                    .padding(.top, 10)
-                                    .padding(.leading, 8)
-
-                                Spacer()
-                            }
-                            Spacer()
-                        }
-                    }
-                }
+                TaskTitleSection(title: $title, showIconPicker: $showIconPicker, selectedIconName: $selectedIconName, iconColor: $iconColor)
+                DueDateSection(dueDate: $dueDate, isTime: $isTime)
+                ColorSection(iconColor: $iconColor)
+                NotesSection(notes: $notes)
             }
             .scrollIndicators(.hidden)
             
             Spacer()
             
-            Button(action: submitForm) {
-                Text("Submit")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .frame(maxWidth: .infinity, minHeight: 44)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
+            SubmitButton(action: submitForm)
         }
         .onTapGesture {
-            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to:nil, from:nil, for:nil)
+            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
         }
         .padding()
         .navigationTitle("Create Task")
@@ -139,6 +33,171 @@ struct AddTaskView: View {
     func submitForm() {
         // Handle form submission
         print("Date: \(dueDate)")
+    }
+}
+
+struct TaskTitleSection: View {
+    @Binding var title: String
+    @Binding var showIconPicker: Bool
+    @Binding var selectedIconName: String
+    @Binding var iconColor: Color
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            HStack {
+                Image(systemName: "pencil")
+                    .foregroundColor(.blue)
+                Text("Title:")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            
+            HStack {
+                TextField("Enter title", text: $title)
+                    .padding(.leading, 4)
+                    .padding(.bottom, 8)
+                    .overlay(
+                        Rectangle()
+                            .frame(height: 1)
+                            .foregroundColor(Color.primary)
+                            .padding(.leading, 4),
+                        alignment: .bottom
+                    )
+                    .padding(.bottom, 15)
+                
+                Button(action: {
+                    showIconPicker.toggle()
+                }) {
+                    Image(systemName: selectedIconName)
+                        .frame(width: 45, height: 45)
+                        .foregroundStyle(iconColor)
+                        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 10.0))
+                }
+                .padding(.bottom, 15)
+                .popover(isPresented: $showIconPicker) {
+                    IconPickerView(selectedIcon: $selectedIconName, iconColor: $iconColor)
+                }
+            }
+        }
+    }
+}
+
+struct DueDateSection: View {
+    @Binding var dueDate: Date
+    @Binding var isTime: Bool
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            HStack {
+                Image(systemName: "calendar")
+                    .foregroundColor(.blue)
+                Text("Date:")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+                Image(systemName: "clock")
+                    .foregroundStyle(.blue)
+                Text("Time")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Toggle(isOn: $isTime) {}
+                    .toggleStyle(SwitchToggleStyle(tint: .blue))
+                    .scaleEffect(0.8)
+                    .frame(width: 35)
+                    .padding(.trailing, 15)
+            }
+            
+            VStack {
+                DatePicker(
+                    "Select due date",
+                    selection: $dueDate,
+                    displayedComponents: [.date]
+                )
+                .datePickerStyle(WheelDatePickerStyle())
+                .labelsHidden()
+                
+                if isTime {
+                    DatePicker(
+                        "Set a time",
+                        selection: $dueDate,
+                        displayedComponents: [.hourAndMinute]
+                    )
+                    .labelsHidden()
+                }
+            }
+        }
+    }
+}
+
+struct ColorSection: View {
+    @Binding var iconColor: Color
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "paintpalette")
+                .foregroundColor(.blue)
+            Text("Color:")
+                .font(.title2)
+                .fontWeight(.bold)
+            Spacer()
+        }
+    }
+}
+
+struct NotesSection: View {
+    @Binding var notes: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            HStack {
+                Image(systemName: "text.alignleft")
+                    .foregroundColor(.blue)
+                Text("Notes:")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            
+            ZStack {
+                TextEditor(text: $notes)
+                    .padding(2)
+                    .border(Color.gray, width: 2)
+                    .frame(height: 200)
+                    .cornerRadius(3)
+                
+                if notes.isEmpty {
+                    VStack {
+                        HStack {
+                            Text("Enter notes here...")
+                                .foregroundStyle(.tertiary)
+                                .padding(.top, 10)
+                                .padding(.leading, 8)
+                            Spacer()
+                        }
+                        Spacer()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct SubmitButton: View {
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text("Submit")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, minHeight: 44)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
     }
 }
 
